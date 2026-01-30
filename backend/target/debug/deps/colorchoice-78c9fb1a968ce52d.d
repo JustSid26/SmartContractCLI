a2ddbb356ae9152cf6a9c26f8a9ab5a589bda999
justSid26 <lamasidharth25@gmail.com>
2026-01-30 08:21:20 +0530

    Remove Rust target directory from version control TWT

--- backend/target/debug/deps/colorchoice-78c9fb1a968ce52d.d
+++
@@ -1,7 +0,0 @@
-/Users/sid/Desktop/SmartContractCLI/backend/target/debug/deps/colorchoice-78c9fb1a968ce52d.d: /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs
-
-/Users/sid/Desktop/SmartContractCLI/backend/target/debug/deps/libcolorchoice-78c9fb1a968ce52d.rlib: /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs
-
-/Users/sid/Desktop/SmartContractCLI/backend/target/debug/deps/libcolorchoice-78c9fb1a968ce52d.rmeta: /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs
-
-/Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs:
--- backend/target/debug/deps/utf8parse-320b8e8d74e79ccf.d
+++
@@ -1,8 +0,0 @@
-/Users/sid/Desktop/SmartContractCLI/backend/target/debug/deps/utf8parse-320b8e8d74e79ccf.d: /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/lib.rs /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/types.rs
-
-/Users/sid/Desktop/SmartContractCLI/backend/target/debug/deps/libutf8parse-320b8e8d74e79ccf.rlib: /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/lib.rs /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/types.rs
-
-/Users/sid/Desktop/SmartContractCLI/backend/target/debug/deps/libutf8parse-320b8e8d74e79ccf.rmeta: /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/lib.rs /Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/types.rs
-
-/Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/lib.rs:
-/Users/sid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf8parse-0.2.2/src/types.rs:
